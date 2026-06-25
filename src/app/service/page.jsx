fa3ecc0661b02df3fffb7fc1c46de20358a439a8
DarkSeaGul02131232100 <190203857+TahirHameed106@users.noreply.github.com>
2026-06-25 19:51:29 +0500

    Day 3: service and seller profile code and structure completed

---
+++ src/app/service/page.jsx
@@ -0,0 +1,13 @@
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import ServicesDetails from '@/components/ServicesDetails';
+
+export default function ServicePage() {
+  return (
+    <>
+      <Navbar />
+      <ServicesDetails />
+      <Footer />
+    </>
+  );
+}
