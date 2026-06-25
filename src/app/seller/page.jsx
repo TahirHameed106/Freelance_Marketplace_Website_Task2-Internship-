cd1a7f7238c52cd269751fc79d67a5a3db435bc5
DarkSeaGul02131232100 <190203857+TahirHameed106@users.noreply.github.com>
2026-06-25 19:50:33 +0500

    Day 3: service and seller profile blueprint completed

---
+++ src/app/seller/page.jsx
@@ -0,0 +1,13 @@
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import SellerProfile from '@/components/SellerProfile';
+
+export default function SellerPage() {
+  return (
+    <>
+      <Navbar />
+      <SellerProfile />
+      <Footer />
+    </>
+  );
+}
