78be12b8582fe15cc2680b100dc24046ea99eb71
DarkSeaGul02131232100 <190203857+TahirHameed106@users.noreply.github.com>
2026-06-25 20:04:42 +0500

    Day 4: solve a little bugs issues in components routing

--- src/components/ServicesDetails.jsx
+++ src/components/ServicesDetails.jsx
@@ -1,8 +1,6 @@
-import { useParams } from "react-router-dom";
 import { getServiceById } from "../data/sellers";
 
 export default function ServiceDetails() {
-  const { serviceId } = useParams();
 
   const service = getServiceById(serviceId);
 
